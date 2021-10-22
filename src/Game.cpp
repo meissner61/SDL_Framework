c49e640caa6860cbb496c95d817786fc4ffb6a30
Vlad Lopatinsky <vlad.lopatinsky@gmail.com>
2021-10-22 14:38:27 -0700

    disabled screenshot code for now

--- src/Game.cpp
+++ src/Game.cpp
@@ -169,10 +169,10 @@ void Game::Render()
 void Game::Destroy()
 {
 	//ScreenShot Code
-	SDL_Surface* sshot = SDL_CreateRGBSurface(0, 800, 600, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
-	SDL_RenderReadPixels(renderer, NULL, SDL_PIXELFORMAT_ARGB8888, sshot->pixels, sshot->pitch);
-	SDL_SaveBMP(sshot, "screenshot.bmp");
-	SDL_FreeSurface(sshot);
+	//SDL_Surface* sshot = SDL_CreateRGBSurface(0, 800, 600, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+	//SDL_RenderReadPixels(renderer, NULL, SDL_PIXELFORMAT_ARGB8888, sshot->pixels, sshot->pitch);
+	//SDL_SaveBMP(sshot, "screenshot.bmp");
+	//SDL_FreeSurface(sshot);
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
